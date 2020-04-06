2d7c4b80cd678f69fd8a2eb57b8101b001372a0d
jagxshukla <62361078+jagxshukla@users.noreply.github.com>
2020-04-06 13:30:27 +0100

    Add files via upload

--- Formauthentication.java
+++ Formauthentication.java
@@ -14,21 +14,20 @@ public class Formauthentication {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\jagxs\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://the-internet.herokuapp.com");
-		driver.findElement(By.cssSelector("#content > ul > li:nth-child(21) > a")).click();
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[text()='Form Authentication']")).click();
 		driver.findElement(By.id("username")).sendKeys("tomsmith");
 		driver.findElement(By.id("password")).sendKeys("abc123");
-		driver.findElement(By.cssSelector("#login > button > i")).click();
+		driver.findElement(By.cssSelector(".radius")).click();
 		String actualError1=driver.findElement(By.id("flash")).getText();
-		System.out.println(actualError1);
 		String expectedError1="Your password is invalid!";
 		Assert.assertTrue(actualError1.contains(expectedError1));
 		
 		//Test case 1 Scenario 2
 		driver.findElement(By.id("username")).sendKeys("abcxyz");
 		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-		driver.findElement(By.cssSelector("#login > button > i")).click();
+		driver.findElement(By.cssSelector(".radius")).click();
 		String actualError2=driver.findElement(By.id("flash")).getText();
-		System.out.println(actualError2);
 		String expectedError2="Your username is invalid!";
 		Assert.assertTrue(actualError2.contains(expectedError2));
 		
@@ -36,9 +35,10 @@ public class Formauthentication {
 		//Test case 1 Scenario 3
 		driver.findElement(By.id("username")).sendKeys("tomsmith");
 		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-		driver.findElement(By.cssSelector("#login > button > i")).click();
-		driver.findElement(By.cssSelector("#content > div > a > i")).click();
-		System.out.println("All Tests Passed");
+		driver.findElement(By.cssSelector(".radius")).click();
+		driver.findElement(By.cssSelector(".button.secondary.radius")).click();
+		System.out.println("Test 1 Passed");
+		driver.quit();
 	}
 }
 	
